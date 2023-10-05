9f316b1cbdf22365c9e91b01865b9cb25cb2347f
priyo105 <adnankamal972@gmail.com>
2023-10-05 09:49:57 +0100

    error fix

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -9,7 +9,7 @@ import ReactStars from "react-rating-stars-component";
 
 export default function Details(params:any) {
     const product = params.searchParams;
-    const slides = params.searchParams.images.length>0?params.searchParams.images.map(item => ({ "url": item })):""  ;
+    const slides = params.searchParams.images.length>0?params.searchParams.images.map((item: any) => ({ "url": item })):""  ;
     const containerStyles = {
         width: "80%",
         height: "400px",
